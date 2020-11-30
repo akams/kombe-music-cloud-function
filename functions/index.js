27d007d241559980ecdb8875e00e6b5b311329ac
akams <e.ndjawe@gmail.com>
2020-11-30 22:14:09 +0100

    feat: add feature get albums, update archi files, directory

--- functions/index.js
+++ functions/index.js
@@ -4,10 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')();
 const cors = require('cors')({origin: true});
+const modelMusics = require('./models/musics');
 
 admin.initializeApp(functions.config().firebase);
 
-const db = admin.firestore(); // cloudFireStore Db
 const app = express(); // Handle intern API
 const main = express(); // Expose API
 
@@ -27,54 +27,21 @@ app.get('/warmup', (req, res) => {
 app.get('/get-musics', async (request, response) => {
   try {
     const bucket = admin.storage().bucket('kombe-music.appspot.com');
-    const { id, limit = 10 } = request.query;
+    const db = admin.firestore();
 
-    const snapshotAlbums = await db.collection('albums').get();
-    const albums = [];
-    snapshotAlbums.forEach((doc) => {
-      albums.push({
-        id: doc.id,
-        ...doc.data(),
-      });
-    });
+    const data = await modelMusics.handleGetMusics(db, bucket, request.query)
+    response.json(data);
+  }
+  catch(error) {
+    response.status(500).send({ err: error.message });
+  }
+});
 
-    let query = db.collection('music');
-    if (id) {
-      query = db.collection('music').where('id', '==', id);
-    }
-
-    const snapshot = await query.orderBy('uploadAt', 'desc').limit(limit).get();
-    const datas = [];
-    snapshot.forEach((doc) => {
-      datas.push({
-        id: doc.id,
-        ...doc.data(),
-      });
-    });
-
-    albums.forEach((album) => {
-      datas.forEach((data) => {
-        if (data.album === album.id) {
-          data.albumName = album.name;
-          data.pathName = `${album.name}/${data.fileName}`;
-        } else {
-          data.pathName = data.fileName;
-        }
-      });
-    });
-  
-    const reads = datas.map((data) => bucket.file(`music/${data.pathName}`).getMetadata())
-    const results = await Promise.all(reads);
-    results.forEach((file) => {
-      const name = file[0].name;
-      const audioUrl = file[0].mediaLink;
-      datas.forEach((data) => {
-        if (name.indexOf(data.fileName) !== -1) {
-          data.audioUrl = audioUrl;
-        }
-      });
-    });
-    response.json(datas);
+app.get('/get-albums', async (request, response) => {
+  try {
+    const db = admin.firestore();
+    const data = await modelMusics.handleGetAlbums(db, request.query)
+    response.json(data);
   }
   catch(error) {
     response.status(500).send({ err: error.message });
---
+++ functions/models/musics.js
@@ -0,0 +1,82 @@
+
+async function handleGetMusics(db, bucket, queryBody) {
+  try {
+    if (!bucket || !db) {
+      throw new Error('{bucket} bucket or {db} db is required to continue the process');
+    }
+    const { id, limit = 10 } = queryBody;
+
+    const snapshotAlbums = await db.collection('albums').get();
+    const albums = [];
+    snapshotAlbums.forEach((doc) => {
+      albums.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    let query = db.collection('music');
+    if (id) {
+      query = db.collection('music').where('id', '==', id);
+    }
+
+    const snapshot = await query.orderBy('uploadAt', 'desc').limit(limit).get();
+    const datas = [];
+    snapshot.forEach((doc) => {
+      datas.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    albums.forEach((album) => {
+      datas.forEach((data) => {
+        if (data.album === album.id) {
+          data.albumName = album.name;
+          data.pathName = `${album.name}/${data.fileName}`;
+        } else {
+          data.pathName = data.fileName;
+        }
+      });
+    });
+
+    const reads = datas.map((data) => bucket.file(`music/${data.pathName}`).getMetadata())
+    const results = await Promise.all(reads);
+    results.forEach((file) => {
+      const name = file[0].name;
+      const audioUrl = file[0].mediaLink;
+      datas.forEach((data) => {
+        if (name.indexOf(data.fileName) !== -1) {
+          data.audioUrl = audioUrl;
+        }
+      });
+    });
+    return datas;
+  } catch (error) {
+    return error;
+  }
+}
+
+async function handleGetAlbums(db, queryBody) {
+  try {
+    if (!db) {
+      throw new Error('{db} db is required to continue the process');
+    }
+    const { limit = 10 } = queryBody;
+
+    const albums = [];
+    const snapshot = await db.collection('albums').orderBy('uploadAt', 'desc').limit(limit).get();
+    snapshot.forEach((doc) => {
+      albums.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+    return albums;
+  } catch (error) {
+    return error;
+  }
+}
+
+exports.handleGetMusics = handleGetMusics;
+exports.handleGetAlbums = handleGetAlbums;
