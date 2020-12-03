dfd95c686c9689e3a61020543995424a2a4cab99
akams <e.ndjawe@gmail.com>
2020-12-03 11:13:27 +0100

    feat: update feature get music for take in charge pagination

--- functions/models/musics.js
+++ functions/models/musics.js
@@ -4,9 +4,11 @@ async function handleGetMusics(db, optionsStorage, queryBody) {
     if (!optionsStorage || !db) {
       throw new Error('{optionsStorage} storage or {db} db is required to continue the process');
     }
-    const { idAlbum, limit = 10 } = queryBody;
+    const { idAlbum, limit = 9, lastVisible } = queryBody;
     const { storage, options } = optionsStorage;
+    const field = 'uploadAt';
 
+    // Recupère les albums
     const snapshotAlbums = await db.collection('albums').get();
     const albums = [];
     snapshotAlbums.forEach((doc) => {
@@ -16,13 +18,26 @@ async function handleGetMusics(db, optionsStorage, queryBody) {
       });
     });
 
+    // Recupère les musics
     let query = db.collection('music');
+    // filtre par id
     if (idAlbum && idAlbum !== 'all') {
       query = db.collection('music').where('album', '==', idAlbum);
     }
 
-    const snapshot = await query.orderBy('uploadAt', 'desc').limit(parseInt(limit)).get();
-    const datas = [];
+    // order by par date
+    query = query.orderBy(field, 'desc');
+
+    // start pagination by par date
+    if(lastVisible) {
+      const parse = JSON.parse(lastVisible);
+      const time = new Date(parse[field]._seconds * 1000);
+      parse[field] = time;
+      query = query.startAfter(parse[field]);
+    }
+
+    const snapshot = await query.limit(parseInt(limit)).get();
+    let datas = [];
     snapshot.forEach((doc) => {
       datas.push({
         id: doc.id,
@@ -30,15 +45,29 @@ async function handleGetMusics(db, optionsStorage, queryBody) {
       });
     });
 
-    albums.forEach((album) => {
-      datas.forEach((data) => {
-        if (data.album === album.id) {
-          data.albumName = album.name;
-          data.pathName = `${album.name}/${data.fileName}`;
+    // last item
+    const last = {
+      ...datas[datas.length - 1],
+    };
+
+    // rajoute nouvelle key
+    datas = datas.map((d) => {
+      const o = albums.map((album) => {
+        if (d.album === album.id) {
+          return {
+            albumName: album.name,
+            pathName: `${album.name}/${d.fileName}`,
+          }
         }
+        return undefined;
       });
+      return {
+        ...d,
+        ...o[0],
+      }
     });
 
+    // récupère les paths file 
     const reads = datas.map((data) => {
       if (data.pathName) {
         return storage
@@ -51,21 +80,22 @@ async function handleGetMusics(db, optionsStorage, queryBody) {
           .file(`music/${data.fileName}`)
           .getSignedUrl(options);
     })
+
+    // ajoute lurl signer pour chaque element avec le même nom
     const results = await Promise.all(reads);
     results.forEach((res) => {
       const url = res[0];
       datas.forEach((data) => {
         const c = data.fileName.split(" ");
         const foundSize = c.filter((cc) => url.search(cc) !== -1).length;
-        console.log
         if (foundSize === c.length) {
           data.audioUrl = url;
         }
       });
     });
-    return datas;
+    return {datas, last};
   } catch (error) {
-    return error;
+    throw error;
   }
 }
 
